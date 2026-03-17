15e106cf69a02deadb7f7f0d5f70884ccd372e4a
Flavio Roman Goncalez <romanflavio3@hotmail.com>
2026-03-16 22:27:47 -0300

    Feat: Desarrollo del homepage mas implementacion de posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import {  Schibsted_Grotesk, Martian_Mono, Geist } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import LightRays from "@/components/LightRays";
+import Navbar from "@/components/Navbar";
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
@@ -48,6 +49,7 @@ export default function RootLayout({
           />
         </div>
         <main>
+          <Navbar/>
           {children}
         </main>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
+import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
+import { events } from "@/lib/events";
 
 export default function Home() {
   return (
     <section>
-      <h1 className="text-center">The Hub for Every <br /> Event You Can't Miss</h1>
+      <h1 className="text-center mt-2">The Hub for Every <br /> Event You Can't Miss</h1>
       <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
       <ExploreBtn/>
+      <div className="mt-20 space-y-7">
+        <h3>Feature Events</h3>
+        <ul className="events">
+          {events.map((event) => (
+            <li key={event.slug}>
+              <EventCard {...event}/>
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
